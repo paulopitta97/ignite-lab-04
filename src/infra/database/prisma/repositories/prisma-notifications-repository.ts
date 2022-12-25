12e46f6e007c73cbb47c9a474fadf04ca123eaee
Paulo Pitta Notebook <paulopitta97@gmail.com>
2022-12-25 03:03:23 -0300

    Implementa listagem por recipiente

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -24,4 +24,12 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
         throw new Error("Method not implemented.");
     }
 
+    async countManyByRecipientId(recipientId: string): Promise<number> {
+        throw new Error("Method not implemented.");
+    }
+
+    async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+        throw new Error("Method not implemented.");
+    }
+
 }
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -30,4 +30,8 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
     async countManyByRecipientId(recipientId: string): Promise<number> {
         return this.notifications.filter(notification => notification.recipientId === recipientId).length;
     }
+
+    async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+        return this.notifications.filter(notification => notification.recipientId === recipientId);
+    }
 }
